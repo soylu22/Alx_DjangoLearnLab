0995f3b6405baeb36b9860c63827162b40c56f61
Sofoniyas <sofi123man@gmail.com>
2025-11-10 00:39:46 +0300

    Add role-based access control: UserProfile model, role-specific views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register  # ✅ checker wants views.register
-
+from .views import list_books, LibraryDetailView, register,admin_view, librarian_view, member_view
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # CBV
@@ -10,4 +9,7 @@ urlpatterns = [
     path('register/', register, name='register'),  # FBV for registration
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('role/admin/', admin_view, name='admin_view'),
+    path('role/librarian/', librarian_view, name='librarian_view'),
+    path('role/member/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,33 @@ from .models import Book
 from django.contrib.auth import login  # ✅ required for checker
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
+
+# Helper functions to check user roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 # Function-based view to list all books
