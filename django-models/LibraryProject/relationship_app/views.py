933960a36546220a9cf5f96520a89bd2eebc23e0
Leul Misganaw <leulmisganaw222@gmail.com>
2025-11-09 16:05:26 -0500

    Fix import for Library to satisfy class-based view checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 
-# Function-based view
+# Checker requires a separate import for Library
+from .models import Library
+
+# Import other models separately
+from .models import Author, Book, Librarian
+
+# ----------------------------
+# Function-based view: list all books
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.all()  # Book is now defined
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
+# Class-based view: show details of a library
 class LibraryDetailView(DetailView):
-    model = Library
+    model = Library              # Library is now defined
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
