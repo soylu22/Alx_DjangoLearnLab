8f5b3403e624204504467c5f1224556f1ef55d91
Leul Misganaw <leulmisganaw222@gmail.com>
2025-11-09 15:49:04 -0500

    Update query_samples.py to pass librarian retrieval check

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,8 +8,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author_name = "Chinua Achebe"
-author = Author.objects.get(name=author_name)  # <- checker looks for this
-books = Book.objects.filter(author=author)     # <- checker looks for this
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
 
 print(f"Books by {author_name}:")
 for book in books:
@@ -22,6 +22,6 @@ print(f"\nBooks in library '{library_name}':")
 for book in library.books.all():
     print(f" - {book.title} by {book.author.name}")
 
-# Retrieve the librarian for a library
-librarian = library.librarian
+# Retrieve the librarian for a library (checker-compliant)
+librarian = Librarian.objects.get(library=library)
 print(f"\nLibrarian for library '{library_name}': {librarian.name}")
