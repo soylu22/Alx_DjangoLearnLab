0779d24a52bdb746e94af107dd56663b511c64eb
Sofoniyas <sofi123man@gmail.com>
2025-11-10 00:01:52 +0300

    Completed both views and templates for library project

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,9 +8,14 @@ from django.views.generic.detail import DetailView
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
