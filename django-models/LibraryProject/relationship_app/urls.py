efa8da6bead223465f63f5683877ed68609b2cb5
Leul Misganaw <leulmisganaw222@gmail.com>
2025-11-09 16:16:26 -0500

    Add all URL patterns including authentication, function- and class-based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,18 @@
+# LibraryProject/relationship_app/urls.py
+
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import register_view, login_view, logout_view  # authentication views
+from .views import list_books, LibraryDetailView, register_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    # Book views
     path('books/', list_books, name='list_books'),
+
+    # Library views
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    
-    # Authentication URLs
+
+    # User authentication
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
