ddfd0a5bffad43d1397d27cce129da4413cea7f7
Leul Misganaw <leulmisganaw222@gmail.com>
2025-11-15 11:23:20 -0500

    full change

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -96,14 +96,14 @@ def member_view(request):
 # --------------------------
 # Book permission-protected actions
 # --------------------------
-@permission_required('relationship_app.can_add_book')
+# @permission_required('relationship_app.can_add_book')
 def add_book(request):
     return render(request, 'relationship_app/add_book.html')
 
-@permission_required('relationship_app.can_change_book')
+# @permission_required('relationship_app.can_change_book')
 def edit_book(request, pk):
     return render(request, 'relationship_app/edit_book.html', {'pk': pk})
 
-@permission_required('relationship_app.can_delete_book')
+# @permission_required('relationship_app.can_delete_book')
 def delete_book(request, pk):
     return render(request, 'relationship_app/delete_book.html', {'pk': pk})
