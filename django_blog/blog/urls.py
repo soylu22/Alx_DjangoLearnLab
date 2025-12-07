0335042ead078bdf664beb5a22da528dde0ea0b0
Leul Misganaw <leulmisganaw222@gmail.com>
2025-12-07 10:14:31 -0500

    adding comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,7 +23,7 @@ urlpatterns += [
 from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns += [
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -125,10 +125,11 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        post_id = self.kwargs['post_id']
-        form.instance.post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.post = post
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
